0cf7162d84fa1e4b82d74b3c639c7bf81fc11c16
ksmahesh <mahesh.kothagere@gmail.com>
2016-10-14 19:58:07 -0500

    Initial commit.
    Lets look at this for foundation of the next project

---
+++ src/aosProject/NodeInfo.java
@@ -0,0 +1,54 @@
+/* AOS Project 1
+ * Submitted by Mahesh Kothagere Siddalingappa 
+ * netId : mxk145330
+ * Course number: 6378
+ * Section: 002
+ * Fall 2016
+ */
+package aosProject;
+
+public class NodeInfo {
+	private int id;
+	private String hostname; 
+	private int portId;
+	private boolean isAvailable;
+	
+	public NodeInfo(int id, String hostname, int portId){
+		this.setId(id);
+		this.setHostname(hostname);
+		this.setPortId(portId);
+		this.setAvailable(true);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getHostname() {
+		return hostname;
+	}
+
+	public void setHostname(String hostname) {
+		this.hostname = hostname;
+	}
+
+	public int getPortId() {
+		return portId;
+	}
+
+	public void setPortId(int portId) {
+		this.portId = portId;
+	}
+
+	public boolean isAvailable() {
+		return isAvailable;
+	}
+
+	public void setAvailable(boolean isAvailable) {
+		this.isAvailable = isAvailable;
+	}
+}
